61f4f2d97d3e515664ba0efab9f1677adb01a546
yuhangzheng94 <61158372+yuhangzheng94@users.noreply.github.com>
2020-10-01 09:55:11 +0800

    revised mini project #2

--- projects/mini-project-2/script.js
+++ projects/mini-project-2/script.js
@@ -7,21 +7,20 @@ let sh = screen.height;
 
 function createHourglass() {
   console.log("create hourglass");
-  console.log("create upper hourglass");
   for (let i=0; i<5; i++) {
-    for (let j = 0; j < 15; j++) {
+    for (let j = 0; j < 16; j++) {
       let randomX = (sw - 160)* ( 1/6 + (1/2 - 1/6)/5 * i) +  Math.random() * (sw - 160) * (2/3 - (1/2 - 1/6)/5 * i * 2);
-      let randomY = Math.random() * (sh - 200) * 1/10 + (sh - 200) * 1/10 * i;
+      let randomY = Math.random() * (sh - 180) * 1/10 + (sh - 180) * 1/10 * i;
       console.log("now a window should open");
-      let newWindow = window.open("sand/index.html","","width=20, height = 100, left = "+randomX+", top = "+randomY);
+      let newWindow = window.open("sand/index.html","","width=100, height = 100, left = "+randomX+", top = "+randomY);
       setTimeout( function(){
         let randomX = (sw - 160)* ( 1/6 + (1/2 - 1/6)/5 * (5-i)) +  Math.random() * (sw - 160) * (2/3 - (1/2 - 1/6)/5 * (5-i) * 2);
-        let randomY = Math.random() * (sh - 200) * 1/10 + (sh - 200) * 1/10 * (i+4);
+        let randomY = Math.random() * (sh - 180) * 1/10 + (sh - 180) * 1/10 * (i+5);
         newWindow.moveTo(randomX, randomY);
-      },(i+1)*10000);
+      },(i+1)*6000);
       setTimeout( function(){
         newWindow.close();
-      },55000);
+      },35000);
     }
   }
 }
