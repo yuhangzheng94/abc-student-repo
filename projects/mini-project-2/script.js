908c804e553f79a5bcc43d466e9a68c425152664
yuhangzheng94 <61158372+yuhangzheng94@users.noreply.github.com>
2020-10-01 10:08:51 +0800

    revised again mini project #2

--- projects/mini-project-2/script.js
+++ projects/mini-project-2/script.js
@@ -10,12 +10,12 @@ function createHourglass() {
   for (let i=0; i<5; i++) {
     for (let j = 0; j < 16; j++) {
       let randomX = (sw - 160)* ( 1/6 + (1/2 - 1/6)/5 * i) +  Math.random() * (sw - 160) * (2/3 - (1/2 - 1/6)/5 * i * 2);
-      let randomY = Math.random() * (sh - 180) * 1/10 + (sh - 180) * 1/10 * i;
+      let randomY = Math.random() * (sh - 120) * 1/10 + (sh - 120) * 1/10 * i;
       console.log("now a window should open");
       let newWindow = window.open("sand/index.html","","width=100, height = 100, left = "+randomX+", top = "+randomY);
       setTimeout( function(){
         let randomX = (sw - 160)* ( 1/6 + (1/2 - 1/6)/5 * (5-i)) +  Math.random() * (sw - 160) * (2/3 - (1/2 - 1/6)/5 * (5-i) * 2);
-        let randomY = Math.random() * (sh - 180) * 1/10 + (sh - 180) * 1/10 * (i+5);
+        let randomY = Math.random() * (sh - 120) * 1/10 + (sh - 120) * 1/10 * (i+5);
         newWindow.moveTo(randomX, randomY);
       },(i+1)*6000);
       setTimeout( function(){
