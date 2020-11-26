7a30fda9adacdb2a74e2781b12d0a55a983841d0
yuhangzheng94 <61158372+yuhangzheng94@users.noreply.github.com>
2020-11-26 09:37:26 +0800

    did some slight change

--- projects/mini-project-7/public/script.js
+++ projects/mini-project-7/public/script.js
@@ -24,6 +24,7 @@ socket.on("deleteBox", (boxId) => {
 
 function addBox(boxAttributes) {
   let box = document.createElement("div");
+  box.class = "box";
   box.id = boxAttributes["boxId"];
   let bt = boxAttributes["boxTop"];
   let bl = boxAttributes["boxLeft"];
